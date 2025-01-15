29445763e5279627c775bec582e40e979adf1771
Rostyslav Hubariev <r.gubarev1303@gmail.com>
2025-01-15 12:57:22 +0200

    Privacy policy added Number format changed Favicon

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,18 +1,24 @@
 var slider = document.getElementById("range-slider");
 var output = document.getElementById("selected-value");
-var finalValue = document.getElementById("final-value");
-output.innerHTML = slider.value;
+var finalValueElement = document.getElementById("final-value");
+
+// Встановлення початкового значення для обраного числа
+output.innerHTML = parseInt(slider.value).toLocaleString('en-US');
 
 slider.oninput = function() {
-    output.innerHTML = this.value;
+    // Форматування обраного значення
+    output.innerHTML = parseInt(this.value).toLocaleString('en-US');
+
+    // Обчислення фінального значення залежно від діапазону
+    var finalValue;
     if (this.value < 30000) {
-        var finalValue = Math.floor(this.value * 0.55);
-        document.getElementById("final-value").innerHTML = finalValue;
+        finalValue = Math.floor(this.value * 0.55);
     } else if (this.value >= 30000 && this.value < 70000) {
-        var finalValue = Math.floor(this.value * 0.49);
-        document.getElementById("final-value").innerHTML = finalValue;
+        finalValue = Math.floor(this.value * 0.49);
     } else if (this.value >= 70000 && this.value <= 100000) {
-        var finalValue = Math.floor(this.value * 0.39);
-        document.getElementById("final-value").innerHTML = finalValue;
+        finalValue = Math.floor(this.value * 0.39);
     }
-}
+
+    // Відображення фінального значення з форматом чисел
+    finalValueElement.innerHTML = finalValue.toLocaleString('en-US');
+};
